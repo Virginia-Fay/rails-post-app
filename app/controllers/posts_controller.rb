30df499dd47604f8f4a63eb5ad342dd58b7aa4db
virginie lty <virginielty@gmail.com>
2022-06-19 20:53:32 +0200

    partial _form.html.erb and correction redirect_to in posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,7 +29,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
 
     if @post.update(post_params)
-      redirect to @post
+      redirect_to @post
     else
       render 'edit'
     end

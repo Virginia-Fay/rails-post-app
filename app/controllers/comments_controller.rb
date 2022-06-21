d4711d5aa6cd1a2dfebcc8e5cac0ec541c082eb4
virginie lty <virginielty@gmail.com>
2022-06-21 21:49:25 +0200

    view partial comments/_comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create((params[:comment]).permit(:name, :comment))
 
-    redirect_to post_path
+    redirect_to post_path(@post)
   end
 
   def destroy
